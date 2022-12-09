801582e1e3214c10735c0b7b34db3bdfac77fd9d
ngan-ng <ngannguyen1399@gmail.com>
2022-12-09 12:10:15 +0700

    modified HomeController

--- WebClient/Controllers/HomeController.cs
+++ WebClient/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace WebClient.Controllers
             return View();
         }
 
+        public IActionResult Home()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
